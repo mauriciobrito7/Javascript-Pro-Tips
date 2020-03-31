bce733579f3c261e5104fe1e2dc1800af2896ed3
mauriciobrito7 <maubot01@gmail.com>
2020-03-30 23:25:48 -0400

    console

---
+++ consolelog.js
@@ -0,0 +1,34 @@
+const foo = { name: "tom", age: 30, nervous: false };
+const bar = { name: "dick", age: 40, nervous: false };
+const baz = { name: "harry", age: 50, nervous: true };
+
+("Bad Code");
+
+console.log(foo);
+console.log(bar);
+console.log(baz);
+
+("Good Code");
+
+// Computed Property Names
+
+console.log("%c My Friends", "color: orange;");
+console.log({ foo, bar, baz });
+
+console.table([foo, bar, baz]);
+
+// Console.time
+console.time("looper");
+
+let i = 0;
+while (i < 1000000) {
+    i++;
+}
+
+console.timeEnd("looper");
+
+// Stack Trace Logs
+const deleteMe = () => console.trace("bye bye database");
+
+deleteMe();
+deleteMe();
