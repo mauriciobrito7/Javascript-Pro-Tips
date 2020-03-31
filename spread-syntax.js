b5ab266fce44e04a877c56c7b59d47c9d283cc5f
mauriciobrito7 <maubot01@gmail.com>
2020-03-31 00:28:07 -0400

    spread syntax

--- spread-syntax.js
+++ spread-syntax.js
@@ -2,3 +2,36 @@
 
 const pikachu = { name: "Pikachu" };
 const stats = { hp: 40, attack: 60 };
+
+("Bad Object Code");
+
+pikachu["hp"] = stats.hp;
+pikachu["attack"] = stast.attack;
+pikachu["defense"] = stats.defense;
+
+// OR
+const lvl0 = Object.assign(pikachu, stats);
+const lvl1 = Object.assign(pikachu, { hp: 45 });
+
+("Good Object Code");
+const lvl0 = {...pikachu, ...stats };
+const lvl1 = {...pikachu, hp: 45 };
+
+// Arrays
+
+let pokemon = ["Arbok", "Raichu", "Sandshrew"];
+
+("Bad Array Code");
+
+pokemon.push("Bulbasaur");
+pokemon.push("Metapod");
+pokemon.push("Weedle");
+
+("Good Array Code");
+
+// Push
+pokemon = [...pokemon, "Bulbasaur", "Metapod", "Weedle"];
+
+// Shift
+
+pokemon = ["Bulbasaur", "Metapod", "Weedle", ...pokemon];
